53c98d38ea85c6fee34b5687a4dfccc71f6db04d
Yash-Digital-night-owl <yashbaranwal@digitalnightowl.in>
2025-05-08 14:40:52 +0530

    Update cores

--- src/app.js
+++ src/app.js
@@ -8,10 +8,10 @@ config();
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:5173",
     credentials: true
   }))
-  
+
 app.use(express.json());
 app.use(morgan("dev"))
 
