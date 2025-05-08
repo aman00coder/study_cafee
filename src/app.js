27f06b637179888ff08691a173d7a92149a0bb63
Yash-Digital-night-owl <yashbaranwal@digitalnightowl.in>
2025-05-08 15:04:49 +0530

    Update cores

--- src/app.js
+++ src/app.js
@@ -8,10 +8,8 @@ config();
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173', // <-- allow your frontend dev server
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    origin: '*', // Allow all origins
+    credentials: true, // Trying to allow credentials
   }));
   
 app.use(express.json());
