7b2b1418dff349f36acd6cb8a0ba778a4d085f94
Yash-Digital-night-owl <yashbaranwal@digitalnightowl.in>
2025-05-08 15:42:16 +0530

    Update cors

--- src/app.js
+++ src/app.js
@@ -11,9 +11,22 @@ app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true
   }));
+
+   
   
 app.use(express.json());
 app.use(morgan("dev"))
+
+app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+      res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+      res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH");
+      res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+      res.header("Access-Control-Allow-Credentials", "true");
+      return res.sendStatus(200);
+    }
+    next();
+  });
 
 app.use("/api", allRoutes)
 
