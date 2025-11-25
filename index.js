934460cba610fe3e4796b4bcce88bc0eb954479a
er00aman <amandaviet2021@gmail.com>
2025-11-25 16:21:29 +0530

    radha

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
+// index.js
+
+import dotenv from "dotenv";        // dotenv import
+dotenv.config();                    // .env load
+
 import { connect } from "./src/connections/connection.js";
 import app from "./src/app.js";
 
+// Ab PORT environment variable properly read hoga
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 5000
 connect()
-.then(()=>{
+.then(() => {
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
+        console.log('Mongo URI:', process.env.MONGO_URI); // check if env loaded
     });
 })
 .catch((error) => {         
     console.error('Error starting server:', error);
-})
+});
